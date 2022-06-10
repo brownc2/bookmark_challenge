5e5cc0b96569d6ffa16183b1a5095db547fd3df8
Christopher <christopher2brown@hotmail.co.uk>
2022-06-10 12:48:51 +0100

    updated feature test

--- spec/features/deleting_a_bookmark_spec.rb
+++ spec/features/deleting_a_bookmark_spec.rb
@@ -4,7 +4,8 @@ feature 'Deleting a bookmark:' do
     visit('/bookmarks')
     expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
     first('.bookmark').click_button 'Delete'
-    expect(current_path).to eq '/bookmarks/'
+    expect(current_path).to eq '/bookmarks/' #'When we use this test it fails(see below)
+    #expect(page).to have_current_path(/bookmarks/) #however using this line 8 test, it passes,
     expect(page).not_to have_link('Makers Academy', href: 'http://www.makersacademy.com')
   end
 end
